8beddf73412e7fcb0acb17ce6d597a77a5ccb228
maiamburton <44420675+maiamburton@users.noreply.github.com>
2024-04-28 19:47:47 -0700

    Update hangman.js

--- hangMan/hangman.js
+++ hangMan/hangman.js
@@ -1,11 +1,15 @@
 let level = 0;
-let maxErrors = 6;
-let guessesRemaining = 6; // creates a variable to count the number of guesses
+let maxErrors = 7;
+let guessesRemaining = 6;
+let remainingLetters = 0;
 let streakCounter = 0;
 
-let easyWords = ['apple', 'ball', 'gate', 'queen', 'tree', 'blue', 'lion', 'puppy', 
-'shoes', 'rose', 'duck', 'gift', 'king', 'nest', 'star', 'wind', 'ink', 'joy', 
-'mask', 'leaf'];
+let secretWord = "";
+let usedWords = [];
+
+
+let easyWords = ['apple', 'ball', 'cat', 'queen', 'tree', 'ink', 'lion', 'puppy', 
+'rose', 'chair', 'duck', 'boat', 'nest', 'star', 'wind', 'blue', 'joy', 'mask', 'leaf'];
 
 let mediumWords = ['pancake', 'banana', 'wallet', 'orange', 'jungle', 'ladder', 
 'noodle', 'sponge', 'yellow', 'candle', 'magnet', 'grass', 'clock', 'rocket', 'whisper',
@@ -19,43 +23,22 @@ let difficultWords = ['strength', 'abruptly', 'ivory', 'swivel', 'transcript', '
 let difficultyIndex = [easyWords, mediumWords, difficultWords];
 
 let easyBtn = document.getElementById('easy');
-
-
 let mediumBtn = document.getElementById('medium');
-
-
 let difficultBtn = document.getElementById('difficult');
-
+easyBtn.addEventListener('click', () => selectDifficulty(easyBtn, 0));
+mediumBtn.addEventListener('click', () => selectDifficulty(mediumBtn, 1));
+difficultBtn.addEventListener('click', () => selectDifficulty(difficultBtn, 2));
 
 let streak = document.getElementById('streak');
-streak.textContent = streakCounter;
-
-// select a random word from an arraylist
-let secretWord = difficultyIndex[level][Math.floor(Math.random() * difficultyIndex[level].length)].toUpperCase();
-let remainingLetters = secretWord.length;
-document.getElementById('wordLength').textContent = remainingLetters;
-
-// create an arraylist for the number of letters of the word
-var answerArray = [];
-for (let i = 0; i < secretWord.length; i++){
-    answerArray[i] = "_";
-}
-
-// Create blanks for each letter in the secret word
+let startBtn = document.getElementById('startGame');
+let hintBtn = document.getElementById('hintBtn');
+hintBtn.disabled = true;
+let image = document.getElementById('hanging-man');
+let bContainer = document.getElementById('button-container');
 let lContainer = document.getElementById('letter-container');
-for (let i = 0; i < secretWord.length; i++){
-    let newLetter = document.createElement("div");
-    newLetter.classList.add("letterBox");
-    newLetter.classList.add(secretWord[i]);
-    newLetter.textContent = " ";
-
-    lContainer.appendChild(newLetter);
-}
 
 
 // Create 26 buttons for each letter in the alphabet
-let bContainer = document.getElementById('button-container');
-
 for (let i = 0; i < 26; i++){
     let newButton = document.createElement("button");
     newButton.textContent = String.fromCharCode(65 + i);  // ASCII value of 'A' is 65
@@ -70,12 +53,12 @@ for (let i = 0; i < 26; i++){
     newButton.addEventListener("click", function() {
         this.classList.remove("notSelected");
         let selectedLetter = this.textContent;
-        let letterBoxes = lContainer.querySelectorAll('.letterBox');
+        let letterBox = lContainer.querySelectorAll('.letterBox');
         let found = false;
 
-        letterBoxes.forEach(function(letterBox) {
-            if (letterBox.classList.contains(selectedLetter)) {
-                letterBox.textContent = selectedLetter;
+        letterBox.forEach(function(letter) {
+            if (letter.classList.contains(selectedLetter)) {
+                letter.textContent = selectedLetter;
                 found = true;
             }
         })
@@ -86,46 +69,92 @@ for (let i = 0; i < 26; i++){
         } else {
             newButton.classList.add("wrong");
             guessesRemaining--;
-            updateImage();
             if (guessesRemaining <= 0) {
                 gameOver();
+            } else {
+                updateImage();
             }
         }
     });
 }
 
-let imageNames = ["start.png", "1wrong.png", "2wrong.png", "3wrong.png", "4wrong.png", "5wrong.png", "gameOver.png"];
-function updateImage() {
-    let image = document.getElementById('hanging-man');
-    let source = "images/" + imageNames[maxErrors-guessesRemaining];
-    image.src = source;
-}
+function selectDifficulty (selectedButon, levelNum) {
+    let allButtons = [easyBtn, mediumBtn, difficultBtn];
 
-
-function countRemainingLetters(selectedLetter) {
-    for (let i = 0; i < secretWord.length; i++){
-        if (selectedLetter == secretWord[i]) {
-            remainingLetters--;
+    allButtons.forEach(button => {
+        if (button === selectedButon) {
+            button.classList.add('selectedLevel');
+        } else {
+            button.classList.remove('selectedLevel');
         }
-    }
-     
-    alert(remainingLetters);
-    if (remainingLetters <= 0) {
-        let streak = document.getElementById('streak');
-        streakCounter++;
-        streak.textContent = streakCounter;
-    }
+    })
+
+    level = levelNum;
 }
 
-function gameOver() {
-    let hintBtn = document.getElementById('hintBtn');
+
+// Start game
+document.getElementById('startGame').addEventListener('click', initializedGame);
+function initializedGame() {
+    guessesRemaining = maxErrors;
+    image.src = "images/start.png";
+    startBtn.innerHTML = "New Word";
     hintBtn.disabled = true;
+
+    if (difficultyIndex[level].length === 0) {
+        difficultyIndex[level] = usedWords.slice();   // reset if all words are used
+        usedWords = [];
+        alert("All words in this difficulty level have been seen. Resetting words.");
+    }
+
+    // select random index for the word
+    let randomIndex = Math.floor(Math.random() * difficultyIndex[level].length);
+
+    // remove the word from the array and store it
+    secretWord = difficultyIndex[level].splice(randomIndex, 1)[0].toUpperCase();
+
+    document.getElementById('wordLength').textContent = secretWord.length;
+    remainingLetters = secretWord.length;
+    usedWords.push(secretWord)
+    
+    answerArray = [];
+    for (let i = 0; i < secretWord.length; i++){
+        answerArray[i] = "_";
+    }
+
+    // Clear and recreate letter containers
+    let lContainer = document.getElementById('letter-container');
+    lContainer.innerHTML = '';
+    for (let i = 0; i < secretWord.length; i++) {
+        let newLetter = document.createElement("div");
+        newLetter.classList.add("letterBox");
+        newLetter.classList.add(secretWord[i]);
+        newLetter.textContent = " ";
+
+        lContainer.appendChild(newLetter);
+    }
+
+    resetButtonSettings();
+    hintBtn.disabled = false;
+}
+
+function resetButtonSettings() {
+    let buttons = bContainer.querySelectorAll('.letterBtn');
+
+    buttons.forEach(function(button) {
+        button.className = '';
+        button.classList.add('letterBtn', 'notSelected');
+    })
 }
 
 
-let hintBtn = document.getElementById('hintBtn');
-hintBtn.addEventListener('click', hintBtnClick);
+let imageNames = ["start.png", "1wrong.png", "2wrong.png", "3wrong.png", "4wrong.png", "5wrong.png", "6wrong.png", "Win.png"];
+function updateImage() {
+    image.src = "images/" + imageNames[maxErrors-guessesRemaining];
+}
 
+
+hintBtn.addEventListener('click', hintBtnClick);
 function hintBtnClick () {
     for (let i = 0; i < secretWord.length; i++){
         if (answerArray[i] == "_") {
@@ -137,3 +166,48 @@ function hintBtnClick () {
         }
     }
 }
+
+
+function countRemainingLetters(selectedLetter) {
+    for (let i = 0; i < secretWord.length; i++){
+        if (selectedLetter == secretWord[i]) {
+            remainingLetters--;
+        }
+    }
+    
+    if (remainingLetters <= 0) {
+        let streak = document.getElementById('streak');
+        streakCounter++;
+        streak.textContent = streakCounter;
+
+        image.src = "images/Win.png";
+        hintBtn.disabled = true;
+        endGameLetterBoxes();
+    }
+}
+
+function gameOver() {
+    image.src = "images/Gameover.png";
+    usedWords.push(secretWord);
+    streak.textContent = 0;
+    hintBtn.disabled = true;
+    endGameLetterBoxes();
+    startBtn.innerHTML = "Next Round";
+}
+
+function endGameLetterBoxes() {
+    // Iterate through letter boxes
+    let letterBox = lContainer.querySelectorAll('.letterBox');
+    letterBox.forEach(function(letter) {
+    
+        // Iterate through the letter's class names and find the single character name
+        Array.from(letter.classList).forEach(function(className) {
+            if (className.length === 1) {
+                letter.innerHTML = className;
+            }
+        })
+        letter.style.border = 'none';
+        letter.style.fontSize = '20px';
+    })
+}
+
